34f3b0cd8c9297109b1fc19d1362c425c8b95188
Ryan Walker <ryanw1470@icloud.com>
2025-11-05 17:42:26 -0600

    Add initial TeleOp mode class
    Introduces a new TeleOp class for FTC robot control, marked as @Disabled and with empty init and loop methods. This sets up the structure for future teleoperated control logic.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+@Disabled
+
+@com.qualcomm.robotcore.eventloop.opmode.TeleOp (name = "Drive")
+public class TeleOp extends OpMode {
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
